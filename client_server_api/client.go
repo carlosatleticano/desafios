3ab27799eee5899e16c1bf31201660934a7ca29f
carlos oliveira de sousa <carlosatleticano@gmail.com>
2024-10-18 16:41:04 -0300

    Update client.go

--- client_server_api/client.go
+++ client_server_api/client.go
@@ -21,15 +21,21 @@ func main() {
 	ctx, cancel := context.WithTimeout(ctx, 300 * time.Millisecond)
 	defer cancel()
 
-    requisição, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080/cotacao", nil)
+    requisição, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8070/cotacao", nil)
     if err != nil {        log.Fatalf("Erro ao fazer a requisição: %v", err)    }
     resposta, err := http.DefaultClient.Do(requisição)
-    if err != nil {        log.Fatalf("Erro ao recuperar a resposta da requisição: %v", err)    }
+    if err != nil {       
+		 println("Erro: Contexto da solicitacao ao servidor excedido:")  
+		 return  
+	}
     defer resposta.Body.Close()
     corpo, err := io.ReadAll(resposta.Body)
     if err != nil {        log.Fatalf("Erro ao ler o corpo da resposta: %v", err)    }
 
     var dados Response
+
+	fmt.Println(string(corpo))
+
     err = json.Unmarshal(corpo, &dados)
     if err != nil {        log.Fatalf("Erro ao fazer o parse da resposta: %v", err)    }
 
@@ -39,5 +45,8 @@ func main() {
 	if err != nil {	fmt.Fprintf(os.Stderr, "Erro ao criar o arquivo: %v\n", err)   }
 	defer file.Close()
 	_, err = file.WriteString(fmt.Sprintf("Dólar: %s", str2))
+	
 }
+
+
 
